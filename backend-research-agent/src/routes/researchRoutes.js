4c12d837476c196a0882b48189413a06b03b745d
Het Prashant Patel <hetpatel1b@gmail.com>
2026-05-19 16:36:11 +0530

    Delete backend-research-agent directory

--- backend-research-agent/src/routes/researchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createResearch } = require("../controllers/researchController");
-
-const router = express.Router();
-
-router.post("/", createResearch);
-
-module.exports = router;
